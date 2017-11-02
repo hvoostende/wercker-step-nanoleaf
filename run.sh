f6428b909ef247d51fa7391ae8688e880b7b58cb
Han van Oostende <hvoostende@gmail.com>
2017-11-02 14:35:04 +0100

    save is handy

--- run.sh
+++ run.sh
@@ -6,53 +6,23 @@ if [ -z "$WERCKER_NANOLEAF_NOTIFIER_URL" ]; then
   fail "Please provide a Nanoleaf URL"
 fi
 
-export COLOR="passed"
+export COLOR="Passed"
 
 if [ -z "$WERCKER_RESULT" ]; then
   echo "WERCKER RESULT: PASSED"
-  export COLOR="deploying"
+  export COLOR="Deploying"
 fi
 
 if [ "$WERCKER_RESULT" = "failed" ]; then
   echo "WERCKER RESULT: FAILED"
-  export COLOR="failed"
+  export COLOR="Failed"
 fi
 
 # construct the json
-# json=$'{"project":"'"$WERCKER_NANOLEAF_NOTIFIER_PROJECT"'","color":"'"$COLOR"'"}'
-# echo $json
-
-# construct the json
 json="{
-    \"project\": \"PP2\",
-    \"color\":\"$COLOR\"
+    \"project\": \"$WERCKER_NANOLEAF_NOTIFIER_PROJECT\",
+    \"color\": \"$COLOR\"
 }"
 
-# post the result to the nanoleaf webhook
 echo "Post the result to the nanoleaf webhook"
-echo "[$json] [$WERCKER_NANOLEAF_NOTIFIER_URL] Content-Type: application/json"
 curl -d "$json" "$WERCKER_NANOLEAF_NOTIFIER_URL" -H "Content-Type: application/json" -v
-echo "Posted!"
-# cat "$WERCKER_STEP_TEMP/result.txt"
-
-# if [ "$RESULT" = "500" ]; then
-#   if grep -Fqx "No token" "$WERCKER_STEP_TEMP/result.txt"; then
-#     fail "No token is specified."
-#   fi
-
-#   if grep -Fqx "No hooks" "$WERCKER_STEP_TEMP/result.txt"; then
-#     fail "No hook can be found for specified subdomain/token"
-#   fi
-
-#   if grep -Fqx "Invalid channel specified" "$WERCKER_STEP_TEMP/result.txt"; then
-#     fail "Could not find specified channel for subdomain/token."
-#   fi
-
-#   if grep -Fqx "No text specified" "$WERCKER_STEP_TEMP/result.txt"; then
-#     fail "No text specified."
-#   fi
-# fi
-
-# if [ "$RESULT" = "404" ]; then
-#   fail "Subdomain or token not found."
-# fi
